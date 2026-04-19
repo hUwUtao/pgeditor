9835be788962ee029cc57c8bba1782641895b8b6
stdpi <git@stdpi.work>
2026-04-19 20:07:28 +0700

    fix: target framebuffer viewport hook via intermediary

--- src/main/java/work/stdpi/pge/editor/mixin/FramebufferMixin.java
+++ src/main/java/work/stdpi/pge/editor/mixin/FramebufferMixin.java
@@ -1,14 +1,13 @@
 package work.stdpi.pge.editor.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gl.Framebuffer;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import work.stdpi.pge.editor.logic.ViewportController;
 
-@Mixin(Framebuffer.class)
+@Mixin(targets = "net.minecraft.class_276")
 public class FramebufferMixin {
     @Inject(method = "method_1235", at = @At("TAIL"))
     private void pge$reapplyViewportAfterFramebufferBind(boolean setViewport, CallbackInfo ci) {
